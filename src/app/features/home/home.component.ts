c43301ec2468dbe7cc92bd36306398e099822237
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-01-16 01:43:10 +0400

    Update HomeComponent to dynamically define the required chessboard size based on the user's screen resolution

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {NavbarComponent} from '../shared/navbar/navbar.component';
 import {ChessBoardComponent} from '../shared/chess-board/chess-board.component';
 
@@ -8,6 +8,22 @@ import {ChessBoardComponent} from '../shared/chess-board/chess-board.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  protected readonly chessBoardSize: number = 420;
+export class HomeComponent implements OnInit {
+  protected chessBoardSize: number = 420;
+
+  ngOnInit(): void {
+    this.updateChessBoardSize();
+  }
+
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    this.updateChessBoardSize();
+  }
+
+  private updateChessBoardSize(): void {
+    const viewportWidth: number = window.innerWidth;
+    const viewportHeight: number = window.innerHeight;
+
+    this.chessBoardSize = Math.min(viewportWidth, viewportHeight) * 0.6;
+  }
 }
