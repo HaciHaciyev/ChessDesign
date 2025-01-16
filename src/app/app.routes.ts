f9a94520c6c8ddc0fc096ca2b4173e8ac12a6370
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-01-16 05:19:06 +0400

    continue to refactor navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
-import {TempComponent} from './features/temp/temp.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "entrance", loadComponent: () => import('./features/entrance/entrance.component').then(m => m.Entrance) },
-  { path: "navbar", component: TempComponent }
 ];
